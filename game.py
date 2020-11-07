6cbe59e8e68f1e18ac6c5a72843f0a7d4d951780
tormobr <tormod.brandshoi@gmail.com>
2020-11-07 20:23:35 +0100

    prims algo now working propperly for maze gen

--- game.py
+++ game.py
@@ -42,8 +42,7 @@ class Node():
         self.dim = dim
         self.value = value
         self.rect = pygame.Rect(x*dim, y*dim, dim-1, dim-1)
-        self.color = COLORS[value]
-        self.fill_val = 0
+        self.fill_val = BLACK
     
     def __str__(self):
         return f"{self.x}, {self.y}"
@@ -51,14 +50,13 @@ class Node():
     def __repr__(self):
         return f"({self.x},{self.y})"
         
-    def update_value(self, x):
-        self.value = x
-        self.color = COLORS[x]
+    def update_value(self, color):
+        self.value = color
 
 class Game():
     def __init__(self):
-        self.w = 800
-        self.h = 800
+        self.w = 820
+        self.h = 820
         self.block_size = 20
         self.nodes = self.create_nodes()
         self.start = self.nodes[1][1]
@@ -78,20 +76,20 @@ class Game():
         for y in range(self.h // self.block_size):
             tmp = []
             for x in range(self.w // self.block_size):
-                val = 1
+                val = WHITE
                 if x == 0 or x == self.w // self.block_size -1 or y == 0 or y == self.h // self.block_size -1:
-                    val = 7
+                    val = GRAY
                 new = Node(x, y, self.block_size, val)
                 tmp.append(new)
             ret.append(tmp)
         return ret
 
     def draw_nodes(self):
-        self.start.update_value(2)
-        self.end.update_value(3)
+        self.start.update_value(RED)
+        self.end.update_value(GREEN)
         for row in self.nodes:
             for n in row:
-                pygame.draw.rect(self.screen, n.color, n.rect)
+                pygame.draw.rect(self.screen, n.value, n.rect)
 
     def check_collision(self, pos, v):
         x, y = pos
@@ -100,21 +98,21 @@ class Game():
         n.update_value(v)
 
         # If start node
-        if v == 2:
-            self.start.update_value(1)
+        if v == RED:
+            self.start.update_value(WHITE)
             self.start = n
 
         # Id end node
-        if v == 3:
-            self.end.update_value(1)
+        if v == GREEN:
+            self.end.update_value(WHITE)
             self.end = n
 
     def backtrack(self, path):
         for n in reversed(path):
-            n.update_value(6)
+            n.update_value(PURPLE)
             self.update_screen(30)
 
-    def update_screen(self, ticks):
+    def update_screen(self, ticks=200):
         self.screen.fill(BLACK)
         self.draw_nodes()
         self.screen.blit(self.text,(self.w//2,0))
@@ -145,10 +143,10 @@ class Game():
                     q.appendleft((n, new_path))
                 else:
                     q.append((n, new_path))
-                n.update_value(4)
+                n.update_value(BLUE)
 
-            self.update_screen(300)
-            current.update_value(5)
+            self.update_screen(ticks=300)
+            current.update_value(YELLOW)
 
     def manhatten(self, n):
         dx = abs(n.x - self.end.x)
@@ -191,12 +189,12 @@ class Game():
                 if n in visited:
                     continue
                 q.append((n, new_path, f+1))
-                n.update_value(4)
+                n.update_value(BLUE)
 
-            self.update_screen(300)
-            current.update_value(5)
+            self.update_screen(ticks=300)
+            current.update_value(YELLOW)
 
-    def get_neighbors(self, n, rand=False, exclude_vals=[0,7], dig=False):
+    def get_neighbors(self, n, rand=False, exclude_vals=[BLACK, GRAY], dig=False, jmp=1):
         dirs = [(1,0), (0,-1), (-1,0), (0,1)]
         if dig:
             dirs.append((1,1))
@@ -204,50 +202,69 @@ class Game():
         ret = []
         x, y = n.x, n.y
         for dx, dy in dirs:
-            new_y = y + dy
-            new_x = x + dx
+            new_y = y + dy*jmp
+            new_x = x + dx*jmp
+            if new_x < 0 or new_x >= self.w // self.block_size:
+                continue
+            elif new_y < 0 or new_y >= self.h // self.block_size:
+                continue
             new = self.nodes[new_y][new_x]
             if new.value not in exclude_vals:
                 ret.append(new)
         return ret
 
     def reset(self, keep_drawing=False):
-        keep_vals = [1, 7]
-        if keep_drawing: keep_vals.append(0)
+        keep_vals = [WHITE, GRAY]
+        if keep_drawing: keep_vals.append(BLACK)
         for row in self.nodes:
             for n in row:
                 if n.value not in keep_vals:
-                    n.update_value(1)
+                    n.update_value(WHITE)
 
     # Draw as somewhat ok grid
     def prim(self):
         for row in self.nodes[1:-1]:
             for n in row[1:-1]:
-                n.update_value(0)
+                n.update_value(BLACK)
         
-        self.update_screen(2)
-        walls = self.get_neighbors(self.start, exclude_vals=[1, 7, 2])
+        self.update_screen(ticks=2)
+        middle_node = self.nodes[len(self.nodes) // 2][len(self.nodes) // 2]
+        #middle_node.update_value(WHITE)
+        walls = self.get_neighbors(self.start, exclude_vals=[WHITE, GRAY, RED], jmp=2)
+        print(walls)
         visited = set()
         while walls:
             random.shuffle(walls)
             current = walls.pop(0)
+            print(current)
             if current in visited:
                 continue
             visited.add(current)
-            #current.update_value(1)
-            neighbors = self.get_neighbors(current, exclude_vals=[0, 7, 3])
-            print(current)
-            print(neighbors)
-            if len(neighbors) == 1:
-                print("this happens")
-                n = neighbors[0]
-                if n.value != 0:
-                    current.update_value(1) 
-                else: continue
-                news = self.get_neighbors(current, exclude_vals=[1,7,2])
-                walls.extend(news)
-                self.update_screen(200)
+              
+            connections  = self.get_neighbors(current, exclude_vals=[BLACK, GRAY, GREEN], jmp=2)
+            print(connections)
+            node = random.choice(connections)
+            avgx = (current.x + node.x) // 2
+            avgy = (current.y + node.y) // 2
 
+            current.update_value(WHITE)
+            self.nodes[avgy][avgx].update_value(WHITE)
+
+            frontiers = self.get_neighbors(current, exclude_vals=[WHITE, GRAY], jmp=2)
+            for n in frontiers:
+                print(n)
+                self.update_screen(ticks=200)
+                if n.value != WHITE and n not in visited:
+                    walls.append(n)
+            #print(current)
+
+            """
+            current.update_value(WHITE) 
+            news = self.get_neighbors(current, exclude_vals=[WHITE, GRAY, RED, GREEN])
+            walls.extend(news)
+            dx = current.x - n.x
+            dy = current.y - n.y
+            """
 
 
     # Main loop while game active
@@ -260,7 +277,7 @@ class Game():
                 self.check_collision(pos, self.fill_val) 
                     
 
-            self.update_screen(300)
+            self.update_screen(ticks=300)
 
     def handle_events(self):
         # Handle events
@@ -276,11 +293,11 @@ class Game():
                 # Set start
                 if e.key == pygame.K_s:
                     pos = pygame.mouse.get_pos()
-                    self.check_collision(pos, 2)
+                    self.check_collision(pos, RED)
                 # Set end
                 if e.key == pygame.K_e:
                     pos = pygame.mouse.get_pos()
-                    self.check_collision(pos, 3)
+                    self.check_collision(pos, GREEN)
                 # BFS
                 if e.key == pygame.K_b:
                     self.bfs()
@@ -305,7 +322,7 @@ class Game():
                     self.h_factor += .1
                     self.text = self.font.render(f"Heuristic: {self.h_factor}", False, WHITE)
             if e.type == pygame.MOUSEBUTTONDOWN:
-                self.fill_val = 1 if e.button == 3 else 0
+                self.fill_val = WHITE if e.button == 3 else BLACK
                 self.mouse_is_down = True
             if e.type == pygame.MOUSEBUTTONUP:
                 self.mouse_is_down = False
